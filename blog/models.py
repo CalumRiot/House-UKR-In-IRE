db4e8a7fe8e3b0a43e72b80237e7023d386d5741
CalumRiot <calumgriffin@yahoo.com>
2023-03-13 13:43:09 +0000

    create base files for editing, create models.py file with basic fields, create base.html with basic fields and home.html which will be extended from base.html and create style.css for editing html layout

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
